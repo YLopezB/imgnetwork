7a3206451a9f15f8eedd40486d68e93c7e41d8ce
YLopezB <andislopez13@gmail.com>
2026-05-29 20:56:47 -0500

    se configura midlewares y routes

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,44 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { engine } from "express-handlebars";
+import { helpers } from "./helpers.js";
+import morgan from "morgan";
+import multer from "multer";
+import express from "express";
+import { routes } from "../routes/index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const upload = multer({ dest: path.join(__dirname, "public/uploads") });
+
 export const config = (app) => {
+  app.set("port", process.env.PORT || 3000);
+  app.set("views", path.join(__dirname, "views"));
+  app.engine(
+    ".hbs",
+    engine({
+      defaultLayout: "main",
+      partialsDir: path.join(app.get("views"), "partials"),
+      layoutsDir: path.join(app.get("views"), "layouts"),
+      extname: ".hbs",
+      helpers: helpers(),
+    }),
+  );
 
-    app.set('port', process.env.PORT || 3000);
+  // Handlebars as the view
+  app.set("view engine", ".hbs");
 
-    return app;
-}
+  // Middleware
+  app.use(morgan("dev"));
+  app.use(upload.single("image"));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  // Routes
+  routes(app);
+
+  // Static files
+  app.use(express.static(path.join(__dirname, "../public")));
+
+  return app;
+};
